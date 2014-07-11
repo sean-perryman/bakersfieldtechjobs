65eb5bac10f17ce6a94f350b57c696397639e48b
Sean Perryman <se@nperryman.com>
2014-07-11 14:49:02 -0700

    Removed routes that don't need to be there, and added a notice to the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   #Company Listing (Add/Remove/Show)
-  resources :companies
+  #resources :companies
   
   # Roadmap Page
-  get 'roadmap', to: 'roadmap#index'
+  #get 'roadmap', to: 'roadmap#index'
   
   # Landing Page
-  get 'landing_page/index'
+  #get 'landing_page/index'
   
   # Contact Form
-  resources "contacts", only: [:new, :create]
+  #resources "contacts", only: [:new, :create]
 
-  resources :jobs
+  #resources :jobs
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
